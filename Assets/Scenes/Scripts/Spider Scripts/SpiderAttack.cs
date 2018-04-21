fce6a4071f40aefbae3e3359b71a214e74a4e4bc
sehiren <33637096+sehiren@users.noreply.github.com>
2018-04-21 18:11:24 +0200

    araña_con_vida_y_ataque

---
+++ Assets/Scenes/Scripts/Spider Scripts/SpiderAttack.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;     
+    public int attackDamage = 10;               
+
+
+    Animator anim;                              
+    GameObject player;                          
+    //PlayerHealth playerHealth;                  
+    SpiderHealth enemyHealth;                    
+    bool playerInRange;                         
+    float timer;                                
+
+
+    void Awake()
+    {
+        // Setting up the references.
+        player = GameObject.FindGameObjectWithTag("Player");
+        //playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<SpiderHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject == player)
+        {
+            playerInRange = true;
+            anim.SetBool("Attack", true);
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = false;
+            anim.SetBool("Attack", false);
+        }
+    }
+
+
+    void Update()
+    {
+        // Add the time since Update was last called to the timer.
+        timer += Time.deltaTime;
+
+        // If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
+        if (timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+        {
+            // ... attack.
+            Attack();
+        }
+
+        // If the player has zero or less health...
+       /* if (playerHealth.currentHealth <= 0)
+        {
+            // ... tell the animator the player is dead.
+            anim.SetTrigger("PlayerDead");
+        }*/
+    }
+
+
+    void Attack()
+    {
+        // Reset the timer.
+        timer = 0f;
+      
+        // If the player has health to lose...
+        /*if (playerHealth.currentHealth > 0)
+        {
+            // ... damage the player.
+            playerHealth.TakeDamage(attackDamage);
+        }*/
+    }
+}
+
---
+++ Assets/Scenes/Scripts/Spider Scripts/SpiderHealth.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderHealth : MonoBehaviour {
+
+    public int currentHealth;
+    public int maxHealth;
+    bool isDead;
+
+    Animator anim;
+    SphereCollider collider; 
+
+    // Use this for initialization
+    void Start ()
+    {
+        maxHealth = 50;
+        currentHealth = maxHealth;
+        isDead = false;
+
+        anim = GetComponent<Animator>();
+        collider = GetComponent<SphereCollider>();
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeDamage(int amount, Vector3 hitPoint)//la llamará el vikingo cuando le de un espadazo y le toque el collider 
+    {
+
+        if (isDead)
+            return;
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        collider.isTrigger = true;
+
+        anim.SetTrigger("Dead");
+        Destroy(gameObject, 4f);
+
+    }
+
+
+}
