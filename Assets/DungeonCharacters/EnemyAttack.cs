e8f4a459cb8d5c71e006f4aa46879d1bdf329bfa
andreabefuen <al341488@uji.es>
2018-04-23 16:23:47 +0200

    Skeleton enemy finished (need some changes)

---
+++ Assets/DungeonCharacters/EnemyAttack.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+    Animator anim;
+    GameObject player;
+    bool playerInRange;
+    float timer;
+
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        anim = GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = false;
+            anim.SetBool("isAttack", false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        timer += Time.deltaTime;
+
+        if (playerInRange)
+        {
+            Attack();
+            
+        }
+        else
+        {
+            anim.SetBool("isAttack", false);
+        }
+	}
+
+    void Attack()
+    {
+        timer = 0f;
+        anim.SetBool("isAttack", true);
+
+        /*
+         *     if(playerHealth.currentHealth > 0)
+        {
+            // ... damage the player.
+            playerHealth.TakeDamage (attackDamage);
+        }
+         */
+    }
+}
---
+++ Assets/DungeonCharacters/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+    Transform player;
+    NavMeshAgent nav;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(player.position);
+        anim.SetBool("isWalking", true);
+	}
+}
