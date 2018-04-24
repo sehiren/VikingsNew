f40860c3272ad8286e516a8bc4f9484879479a97
andreabefuen <al341488@uji.es>
2018-04-24 15:17:45 +0200

    Scriot dialogo sin acabar

--- Assets/Characters/Villager/ActiveChat.cs
+++ Assets/Characters/Villager/ActiveChat.cs
@@ -1,20 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour {
 
-    Transform player;
+    GameObject player;
 
 
 	// Use this for initialization
 	void Start () {
-        
+
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == player)
+        {
+            ActiveFungus();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other == player)
+        {
+
+            DesactiveFungus();
+        }
+    }
+
+    void DesactiveFungus()
+    {
+       
+    }
+
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void ActiveFungus()
+    {
+
+    }
 }
