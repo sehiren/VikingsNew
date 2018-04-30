9aa5fda3269545f3a7138418e208e38d31af915b
andreabefuen <al341488@uji.es>
2018-04-30 16:05:55 +0200

    Dialogos con npcs y el npc te sigue con la mirada, la escena principal más arreglada

--- Assets/Characters/Villager/ActiveChat.cs
+++ Assets/Characters/Villager/ActiveChat.cs
@@ -3,46 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class ActiveChat: MonoBehaviour {
 
     GameObject player;
+    public GameObject dialog;
 
 
-	// Use this for initialization
-	void Start () {
-
-        player = GameObject.FindGameObjectWithTag("Player");
-	}
-    private void OnTriggerEnter(Collider other)
+    // Use this for initialization
+    void Start()
     {
-        if(other == player)
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
         {
-            ActiveFungus();
+            if (Input.GetKeyDown("z"))
+            {
+                dialog.SetActive(true);
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(other == player)
-        {
-
-            DesactiveFungus();
-        }
-    }
-
-    void DesactiveFungus()
-    {
-       
-    }
-
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
-
-    void ActiveFungus()
-    {
-
+        dialog.SetActive(false);
     }
 }
---
+++ Assets/Characters/Villager/LookToPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookToPlayer : MonoBehaviour {
+
+    public GameObject player;
+    GameObject villager;
+    Vector3 playerPosition;
+    Vector3 npcPosition;
+    Vector3 delta;
+    Quaternion rotation;
+
+    // Use this for initialization
+    void Start () {
+
+        playerPosition = player.transform.position;
+        npcPosition = this.transform.position;
+        delta = new Vector3(playerPosition.x - npcPosition.x, 0.0f, playerPosition.z - npcPosition.z);
+
+        rotation = Quaternion.LookRotation(delta);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        playerPosition = player.transform.position;
+        npcPosition = this.transform.position;
+        delta = new Vector3(playerPosition.x - npcPosition.x, 0.0f, playerPosition.z - npcPosition.z);
+
+        rotation = Quaternion.LookRotation(delta);
+        this.transform.rotation = rotation;
+	}
+}
