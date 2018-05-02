402ccb16e5848d1a866f5e35f382c59f48760258
andreabefuen <al341488@uji.es>
2018-05-02 13:00:15 +0200

    Escape Menu hecho YAAAAAS

---
+++ Assets/Scripts/PauseEscape.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseEscape : MonoBehaviour {
+
+    public Transform canvas;
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+		
+	}
+    public void Pause()
+    {
+        if (canvas.gameObject.activeInHierarchy == false)
+        {
+            canvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+            Camera.main.GetComponent<FollowCamara>().enabled = false;
+        }
+        else
+        {
+            canvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+            Camera.main.GetComponent<FollowCamara>().enabled = true;
+        }
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+    public void MenuButton()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
