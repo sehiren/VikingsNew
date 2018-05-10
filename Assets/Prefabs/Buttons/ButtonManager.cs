5eb2418db80823e25627cf93a09ca0f0263dcec4
andreabefuen <al341488@uji.es>
2018-05-10 08:38:53 +0200

    Pantalla de intro y opciones tiene boton creditos (falta hacer los créditos)

--- Assets/Prefabs/Buttons/ButtonManager.cs
+++ Assets/Prefabs/Buttons/ButtonManager.cs
@@ -4,16 +4,17 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class ButtonManager : MonoBehaviour {
+    public Canvas mainCanvas;
+    public Canvas optionCanvas;
+    // Use this for initialization
+    void Start() {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 
     public void NewGame(string newGameLevel)
     {
@@ -23,5 +24,19 @@ public class ButtonManager : MonoBehaviour {
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void OptionsButton(){
+
+        mainCanvas.enabled = false;
+        optionCanvas.enabled = true;
+
+    }
+
+    public void BackButton()
+    {
+        mainCanvas.enabled = true;
+        optionCanvas.enabled = false;
+
     }
 }
---
+++ Assets/Scenes/Intro/IntroChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class IntroChange : MonoBehaviour {
+    float time;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if(time >= 10)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+	}
+}
